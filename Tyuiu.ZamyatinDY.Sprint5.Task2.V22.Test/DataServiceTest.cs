ac62c98a4f487d208d58f07cdbf029e53f56e90e
ZamyatinDmitry <dimazamatin18@gmail.com>
2023-11-19 23:15:33 +0500

    Task2

---
+++ Tyuiu.ZamyatinDY.Sprint5.Task2.V22.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Tyuiu.ZamyatinDY.Sprint5.Task2.V22.Lib;
+namespace Tyuiu.ZamyatinDY.Sprint5.Task2.V22.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = @"C:\Users\User\source\repos\Tyuiu.ZamyatinDY.Sprint5\Tyuiu.ZamyatinDY.Sprint5.Task0.V6.Test\bin\Debug\OutPutFileTask2.csv";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.ZamyatinDY.Sprint5.Task2.V22/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ZamyatinDY.Sprint5.Task2.V22.Lib;
+namespace Tyuiu.ZamyatinDY.Sprint5.Task2.V22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] mtrx = new int[3, 3] { { -8, -1, -9 },
+                                          { 8, 5, 5 },
+                                          { 9, 6, -2 } };
+            int rows = mtrx.GetUpperBound(0) + 1;
+            int columns = mtrx.Length / rows;
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Замятин Д. Ю. | ИИПб 23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись структурированных данных в текстовый файл      *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #22                                                             *");
+            Console.WriteLine("* Выполнил: Замятин Дмитрий Юрьевич | ИИПб 23-3                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 3 на 3 элементов, заполненный        *");
+            Console.WriteLine("* значениями с клавиатуры. Заменить положительные элементы массива на 1,  *");
+            Console.WriteLine("* отрицательные на 0. Результат сохранить в файл OutPutFileTask2.csv и    *");
+            Console.WriteLine("* вывести на консоль.                                                     *");
+            Console.WriteLine("* -8; -1; -9                                                              *");
+            Console.WriteLine("* 8; 5; 5                                                                 *");
+            Console.WriteLine("* 9; 6; -2                                                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{mtrx[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(mtrx);
+
+            Console.WriteLine(" Файл: " + res);
+            Console.WriteLine(" Создан!");
+            Console.ReadKey();
+        }
+    }
+}
